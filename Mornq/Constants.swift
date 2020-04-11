71c8e704bed8cafbd7043b95851ce8f6704a22c2
Diego Chavez <diegchav.me@gmail.com>
2020-04-11 10:12:26 -0500

    Load random quote from firestore

--- Mornq/Constants.swift
+++ Mornq/Constants.swift
@@ -7,3 +7,13 @@
 //
 
 import Foundation
+
+struct K {
+    
+    struct Firestore {
+        static let quotesCollectionName = "quotes"
+        static let quotesTextField = "text"
+        static let quotesAuthorField = "author"
+    }
+    
+}
--- Mornq/Controllers/ViewController.swift
+++ Mornq/Controllers/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
@@ -14,18 +15,44 @@ class ViewController: UIViewController {
     @IBOutlet weak var quoteLabel: UILabel!
     @IBOutlet weak var authorLabel: UILabel!
     
+    let db = Firestore.firestore()
+    
     let quoteManager = QuoteManager()
+    
+    var quotes: [Quote] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = quoteManager.randomColor()
-    
-        let quote = quoteManager.randomQuote()
-        quoteLabel.text = quote.text
-        authorLabel.text = quote.author
+        
+        loadQuotes()
     }
-
+    
+    private func loadQuotes() {
+        db.collection(K.Firestore.quotesCollectionName).getDocuments() { (querySnapshot, error) in
+            if let err = error {
+                print("Error getting documents: \(err)")
+            } else {
+                if let snapshotDocuments = querySnapshot?.documents {
+                    for document in snapshotDocuments {
+                        let data = document.data()
+                        if let quoteText = data[K.Firestore.quotesTextField] as? String, let quoteAuthor = data[K.Firestore.quotesAuthorField] as? String {
+                            let quote = Quote(text: quoteText, author: quoteAuthor)
+                            self.quotes.append(quote)
+                        }
+                    }
+                    
+                    DispatchQueue.main.async {
+                        if let quote = self.quotes.randomElement() {
+                            self.quoteLabel.text = quote.text
+                            self.authorLabel.text = quote.author
+                        }
+                    }
+                }
+            }
+        }
+    }
 
 }
 
